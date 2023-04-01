c0961ec9ac28ed74910f86a6427085661dec8e34
Exoutia <jhav365@gmail.com>
2023-04-01 08:23:26 +0530

    making the function to create new card inside the view

--- cards/views.py
+++ cards/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import (ListView,)
+from django.views.generic import (ListView, CreateView)
 from .models import Card
+from django.urls import reverse_lazy
 
 class CardListView(ListView):
     model = Card
     queryset = model.objects.all().order_by("box", "-date_created")
+
+class CardCreateView(CreateView):
+    model = Card
+    fields = ["question", "answer", "box"]
+    success_url = reverse_lazy("card-create")
