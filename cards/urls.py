8516f247d67555ccdea340ea2ef6ad5f55a942f2
Exoutia <jhav365@gmail.com>
2023-04-01 07:50:24 +0530

    urls price of the url patterns

--- cards/urls.py
+++ cards/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="cards/base.html"), name="home"),
+    path("", views.CardListView.as_view(), name="card-list"),
 ]
