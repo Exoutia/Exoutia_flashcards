8c83ad2b70b84383acf10e4053276f65ac18ba94
Exoutia <jhav365@gmail.com>
2023-04-01 09:03:38 +0530

    creating he link to the update card

--- cards/urls.py
+++ cards/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path("", views.CardListView.as_view(), name="card-list"),
     path("new/", views.CardCreateView.as_view(), name="card-create"),
+    path(
+        "edit/<int:pk>",
+        views.CardUpdateView.as_view(),
+        name="card-update"
+    ),
+
 ]
